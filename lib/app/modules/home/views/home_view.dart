c0148f0336ba8528a90996564fd1663fcbef3e67
Giulliano <mateogiulliano@hotmail.com>
2021-03-14 02:36:56 -0300

    Se agrega informacion de contacto

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,14 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:poli_calc/app/modules/home/controllers/home_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Poli Calculadora'),
+        title: Text('PoliCalc'),
         centerTitle: true,
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.bug_report,
+              ),
+              onPressed: () {
+                Get.defaultDialog(
+                  title: "¡Hola!",
+                  content: Column(
+                    children: [
+                      Text(
+                          "Si encontraste algún error 🤧 o no entendes como funciona que esperas para escribirme 😃."),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: FaIcon(
+                              FontAwesomeIcons.whatsapp,
+                            ),
+                            onPressed: () {
+                              launch("https://wa.me/0983342745");
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.mail,
+                            ),
+                            onPressed: () {
+                              launch(
+                                  "mailto:mateogiulliano@fpuna.edu.py?subject=PoliCalc&body=Hola Giulliano,");
+                            },
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              })
+        ],
       ),
       body: Center(
           child: Column(
